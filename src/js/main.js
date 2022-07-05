a8ede4893f306520870d787bce3fc9dfe760eec2
Bartek <kuzniewski.bar@gmail.com>
2022-07-05 20:07:12 +0200

    added function that show and clear error for photo and voivodeship inputs

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,6 @@ const allInputs = [
 	zipCode,
 	phoneNumber,
 	mail,
-	profilPhoto,
 ];
 
 const sendBtn = document.querySelector('.send');
@@ -30,6 +29,7 @@ const closeModalBtn = document.querySelector('.close');
 
 function clearAllInputs(e) {
 	e.preventDefault();
+	profilPhoto.value = '';
 
 	allInputs.forEach((input) => {
 		input.value = '';
@@ -43,15 +43,56 @@ function clearAllInputs(e) {
 function checkInputsValue(input) {
 	input.forEach((el) => {
 		if (el.value === '') {
-			console.log(`błąd`);
+			showError(el, el.placeholder);
 		} else {
-			console.log(`ok`);
+			clearError(el);
 		}
 	});
 }
 
+function showError(input, msg) {
+	const formBox = input.parentElement;
+	const errorText = formBox.querySelector('.error-text');
+
+	formBox.classList.add('error');
+	errorText.textContent = `Podaj ${msg}`;
+}
+
+function clearError(input) {
+	const formBox = input.parentElement;
+	formBox.classList.remove('error');
+}
+
+function checkPhotoInput(input) {
+	const formBox = input.parentElement;
+	const errorText = formBox.querySelector('.error-text');
+
+	if (input.value === '') {
+		formBox.classList.add('error');
+		errorText.textContent = `Wybierz zdjęcie`;
+	} else {
+		formBox.classList.remove('error');
+	}
+}
+
+function checkVoivodeshipInput(input) {
+	const formBox = input.parentElement;
+	const errorText = formBox.querySelector('.error-text');
+
+	if (input.value === '0') {
+		formBox.classList.add('error');
+		errorText.textContent = `Wybierz województwo`;
+	} else {
+		formBox.classList.remove('error');
+	}
+}
+
 clearBtn.addEventListener('click', clearAllInputs);
+
 sendBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 	checkInputsValue(allInputs);
+	checkPhotoInput(profilPhoto);
+	checkVoivodeshipInput(voivodeship);
+
 });
